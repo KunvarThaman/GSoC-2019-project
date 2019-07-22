1845b70da3251492faf04698160e9afa1ac317ed
KunvarThaman <31417445+KunvarThaman@users.noreply.github.com>
2019-07-22 20:17:02 +0530

    Update text_to_csv.py

--- Spanish files/text_to_csv.py
+++ Spanish files/text_to_csv.py
@@ -1,9 +1,14 @@
 import csv
+import argparse
 
-with open('testfile.txt', 'r') as in_file:
+parser = argparse.ArgumentParser()
+parser.add_argument("-inf", "--input_file", type=str, help="Name of\input file")
+args = parser.parse_args()
+
+with open(args.input_file, 'r') as in_file:
     stripped = (line.strip() for line in in_file)
     lines = (line.split(",") for line in stripped if line)
-    with open('testing.csv', 'w') as out_file:
+    with open('CSV_version.csv', 'w') as out_file:
         writer = csv.writer(out_file)
         writer.writerow(('title', 'intro'))
         writer.writerows(lines)
