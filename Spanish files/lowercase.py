cb6caf24cbeb30d3a92128f122beabec718d2464
KunvarThaman <31417445+KunvarThaman@users.noreply.github.com>
2019-07-24 13:05:50 +0530

    Add files via upload

---
+++ Spanish files/lowercase.py
@@ -0,0 +1,19 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-inf", "--input_file", type=str, help="Name of\
+                    input file")
+parser.add_argument("-t", "--timing", default=0, type=int, help="Option to decide whether\
+                    to include timing info in output")
+args = parser.parse_args
+
+fin = open(args.input_file)
+fout = open(outfile, "w+")
+
+for line in f:
+		line = line.lower()
+	fout.write(line)
+	
+fin.close()
+fout.close()
+	
